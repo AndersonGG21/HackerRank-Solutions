67f234db012b1604fedda13f33c33d4cd46766a7
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-01-11 18:55:24 -0500

    Add description

--- src/LoopsII.java
+++ src/LoopsII.java
@@ -1,3 +1,11 @@
+// Sample Input:    
+//                 2
+//                 0 2 10
+//                 5 3 5
+// Sample Output:
+//                 2 6 14 30 62 126 254 510 1022 2046
+//                 8 14 26 50 98
+
 import java.util.*;
 
 class LoopsII {
