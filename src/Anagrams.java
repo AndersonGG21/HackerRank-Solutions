19cf7053660e3c19910a4d53a4eab5f0d5b3924a
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-10-31 20:44:26 -0500

    Add solution for Anagrams Challenge

---
+++ src/Anagrams.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagrams {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+
+        sc.close();
+
+        System.out.println(isAnagram(a, b) ? "Anagrams" : "Not Anagrams");
+    }
+
+    private static boolean isAnagram(String a, String b) {
+
+        if (a.length() != b.length())
+            return false;
+
+        char[] aChars = a.toLowerCase().toCharArray();
+        char[] bChars = b.toLowerCase().toCharArray();
+
+        Arrays.sort(aChars);
+        Arrays.sort(bChars);
+
+        return Arrays.equals(aChars, bChars);
+    }
+}
